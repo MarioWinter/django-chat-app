e8f6e5f17a0a3f53cd7366d6f7c7f579dff7df1e
Mario Winter <mariowinter.sg@gmail.com>
2024-09-28 22:14:50 +0200

    feat(chat): implement chat message display and sending functionality

--- chat/views.py
+++ chat/views.py
@@ -7,4 +7,5 @@ def index(request):
 		print(request.POST['textmessage'])
 		myChat = Chat.objects.get(id=1)
 		Message.objects.create(text=request.POST['textmessage'],chat=myChat, author=request.user, receiver=request.user)
-	return render(request, 'chat/index.html')
+	chatMessages = Message.objects.filter(chat__id=1)
+	return render(request, 'chat/index.html', {'messages': chatMessages})
