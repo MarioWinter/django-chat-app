be7104e91d696e567ad9f5e3d58aac43e15e2858
Mario Winter <mariowinter.sg@gmail.com>
2024-09-29 14:03:22 +0200

    feat(chat): add login view and URL pattern

--- chat/views.py
+++ chat/views.py
@@ -9,3 +9,6 @@ def index(request):
 		Message.objects.create(text=request.POST['textmessage'],chat=myChat, author=request.user, receiver=request.user)
 	chatMessages = Message.objects.filter(chat__id=1)
 	return render(request, 'chat/index.html', {'messages': chatMessages})
+
+def login(request):
+	return render(request, 'chat/login.html')
